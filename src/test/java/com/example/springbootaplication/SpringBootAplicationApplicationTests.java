17ee54a7e35dbe82f39f193366b7c3b229337b54
Ivan5 <converse_ivan@hotmail.com>
2020-04-08 12:42:48 -0500

    Initial commit to setup the project

---
+++ src/test/java/com/example/springbootaplication/SpringBootAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
